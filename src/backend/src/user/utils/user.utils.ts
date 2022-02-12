17a7e847ace039023fd9239ac65e6afb8ed8a7b4
Hamza Tagrourete <htagrour@e1r1p2.1337.ma>
2022-02-12 16:29:43 +0100

    updates on user and frienship

---
+++ src/backend/src/user/utils/user.utils.ts
@@ -0,0 +1,19 @@
+import { HttpException, HttpStatus } from "@nestjs/common";
+import { extname } from "path/posix";
+import { RequestWithUser } from "src/authentication/Interfaces/requestWithUser.interface";
+
+export const imageFileFilter = (req, file, callback) => {
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+      return callback(new HttpException('Only image files are allowed!', HttpStatus.BAD_REQUEST), false);
+    }
+    callback(null, true);
+  };
+
+  export const editFileName = (req, file, callback) => {
+    const fileExtName = extname(file.originalname);
+    const randomName = Array(4)
+      .fill(null)
+      .map(() => Math.round(Math.random() * 16).toString(16))
+      .join('');
+    callback(null, `${randomName}${fileExtName}`);
+  };
