7ba973a31d26ac816bccd112c80f55990370b2ef
Hamza Tagrourete <htagrour@e1r1p2.1337.ma>
2022-02-19 18:54:09 +0100

    hh

--- src/backend/src/chat/chat.module.ts
+++ src/backend/src/chat/chat.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/authentication/auth.module';
 import { ChatGateway } from './chat.gateway';
 import { ChannelsController } from './controllers/channels.controller';
+import  ChannelEntity from './entites/channel.entity';
 import { TmpChatService } from './services/chat.service';
 
 @Module({
-    imports: [AuthModule],
+    imports: [
+        AuthModule,
+        TypeOrmModule.forFeature([ChannelEntity])
+    ],
     controllers : [ChannelsController],
     providers: [TmpChatService, ChatGateway]
 })
