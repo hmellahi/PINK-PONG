bfde15f626e526fefcc00071a67caeeeb4e8682b
mac <mac@macs-MacBook-Pro.local>
2023-06-07 01:31:57 +0100

    update the script

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 current_ip=$(ipconfig getifaddr en0)
 printf "Current IP: $current_ip"
 
-# Replace '172.16.84.159' with your IP address
-find . -type f -exec perl -pi -e 's/192\.168\.11\.108/'"$current_ip"'/g' {} +
+# Replace '192.168.11.116' with your IP address
+find . -type f -exec perl -pi -e 's/172\.16\.84\.159/'"$current_ip"'/g' {} +
 
 docker-compose up -d --build
